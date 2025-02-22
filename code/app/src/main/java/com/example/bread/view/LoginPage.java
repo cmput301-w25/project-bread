a9cc87378d31d002256591a93954352db3a6b25e
Armaan Katyal <armaankatyal3@gmail.com>
2025-02-22 13:47:04 -0700

    adds new base activities

--- code/app/src/main/java/com/example/bread/view/LoginPage.java
+++ code/app/src/main/java/com/example/bread/view/LoginPage.java
@@ -1,7 +1,5 @@
 package com.example.bread.view;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -12,26 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.bread.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        SharedPreferences preferences =getSharedPreferences("sharedPrefs", MODE_PRIVATE);
-        if (preferences.getString("username", "").isEmpty()) {
-            Intent intent = new Intent(MainActivity.this, LoginPage.class);
-            startActivity(intent);
-        } else {
-            Intent intent = new Intent(MainActivity.this, HomePage.class);
-            startActivity(intent);
-        }
     }
 }
