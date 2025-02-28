3633e0e48a8305618df6cceec739213618dacb2e
Landys Lepine <connectednorth@Landys-Lepine.local>
2025-02-27 21:34:39 -0700

    Resolved conflicts by accepting main's version for some files

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -26,11 +26,6 @@ android {
             )
         }
     }
-
-    buildFeatures{
-        viewBinding = true
-    }
-
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
