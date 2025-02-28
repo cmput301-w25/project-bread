8f13bfa35b2fc98686007db86622eeea2e180369
Landys Lepine <connectednorth@Landys-Lepine.local>
2025-02-27 21:52:41 -0700

    trying to fix pull conflict for 100th time

--- code/app/src/main/java/com/example/bread/view/HomePage.java
+++ code/app/src/main/java/com/example/bread/view/HomePage.java
@@ -1,20 +1,56 @@
 package com.example.bread.view;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.bread.R;
+import com.example.bread.databinding.ActivityHomePageBinding;
+import com.example.bread.databinding.ActivityMainBinding;
 
 public class HomePage extends AppCompatActivity {
 
+    @NonNull ActivityHomePageBinding binding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /*
+         * Task bar implementation
+         * Changes to each corresponding screen based on the icon user taps on
+         * (home, map, etc.)
+         * */
+        binding = ActivityHomePageBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemID()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.map:
+                    break;
+                case R.id.add:
+                    break;
+                case R.id.history:
+                    break;
+                case R.id.profile:
+                    break;
+            }
+
+            return true;
+        });
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -22,5 +58,12 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
