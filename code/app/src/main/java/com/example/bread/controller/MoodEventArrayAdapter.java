5526cd9bc2b54f835b15095a183964907766ad2d
Farhan Khan <khanfsk04@gmail.com>
2025-03-02 05:11:06 -0700

    Assign a consistent color scheme to each predefined emotional state #85

--- code/app/src/main/java/com/example/bread/controller/MoodEventArrayAdapter.java
+++ code/app/src/main/java/com/example/bread/controller/MoodEventArrayAdapter.java
@@ -1,14 +1,18 @@
 package com.example.bread.controller;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.example.bread.model.MoodEvent;
+import com.example.bread.utils.EmotionUtils;
+import com.example.bread.R;
 
 import java.util.ArrayList;
 
@@ -20,6 +24,27 @@ public class MoodEventArrayAdapter extends ArrayAdapter<MoodEvent> {
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext())
+                    .inflate(R.layout.mood_event_list_item, parent, false);
+        }
+
+
+        MoodEvent moodEvent = getItem(position);
+
+        if (moodEvent != null) {
+            TextView emoticonTextView = convertView.findViewById(R.id.emoticon_text_view);
+            TextView titleTextView = convertView.findViewById(R.id.title_text_view);
+            emoticonTextView.setText(EmotionUtils.getEmoticon(moodEvent.getEmotionalState()));
+
+            titleTextView.setText(moodEvent.getTitle());
+
+
+            int colorResId = EmotionUtils.getColorResource(moodEvent.getEmotionalState());
+            convertView.setBackgroundResource(colorResId);
+        }
+
+        return convertView;
     }
 }
