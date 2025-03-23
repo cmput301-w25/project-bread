fa66d559c059528722a941bb505b94cd45871059
Landys Lepine <connectednorth@Landys-Lepine.local>
2025-03-22 18:49:04 -0600

    embarassing mistake

--- code/app/src/androidTest/java/com/example/bread/MoodEventAddTest.java
+++ code/app/src/androidTest/java/com/example/bread/MoodEventAddTest.java
@@ -205,11 +205,7 @@ public class MoodEventAddTest {
 
     @After
     public void tearDownAuth() {
-<<<<<<< HEAD
-        String projectId = "project-db";  //SET TO YOUR PROJECT ID
-=======
         String projectId = BuildConfig.FIREBASE_PROJECT_ID;
->>>>>>> 0b0d127179e3dc0bad5527b4da54b867abc57556
         URL url = null;
         try {
             url = new URL("http://10.0.2.2:9099/emulator/v1/projects/"+projectId+"/accounts");
